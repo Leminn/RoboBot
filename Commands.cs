8aabc0cc3524826e165c27f6885c38f36838f5f7
Leminn <blake.fuller@lemingames.com>
2020-11-10 00:04:16 +0000

    Embed feature added

--- Commands.cs
+++ Commands.cs
@@ -39,25 +39,58 @@ namespace RoboBot
 
 /*
 */
-        [Command("GF1")]
+        [Command("GFZ1")]
         public async Task Greenflower1Records(CommandContext ctx, string character){
+            bool gotLvl = Enums.levelsID.TryGetValue(ctx.Command.Name, out string levelID);
+            
+            bool gotCat = Enums.categoriesID.TryGetValue(character.ToLower(), out string categoryID);
 
-            Leaderboard leaderboard = Program.srcClient.Leaderboards.GetLeaderboardForLevel(
-                Program.srb2Game.ID,
-                "ywejpqld", // GF1
-                "xd1g1j4d", // Sonic
-                5
-            );
-            /*Level level = Program.srb2Game.Levels.ToArray()[1];
-            IEnumerable<Run> characterRuns = level.Runs.Where(run => run.Category.Name.ToLower() == character.ToLower()); //tolower is to make it case insensitive
-            Run[] verifiedRuns = characterRuns.Where(run => run.Status.Type == RunStatusType.Verified).OrderBy(run => run.Times.GameTime.Value).ToArray();*/
-            for (int i = 0; i < leaderboard.Records.Count(); i++)
+            if(gotCat == false || gotLvl == false)
             {
-                await ctx.RespondAsync($"{i + 1}. {leaderboard.Records[i].Player.Name} | {leaderboard.Records[i].Times.GameTime.Value.ToString(Program.timeFormat)} : {leaderboard.Records[i].WebLink.AbsoluteUri}");
+                await ctx.RespondAsync(ctx.Command.Arguments[0].Name);
+                await ctx.RespondAsync("commandName: " + ctx.Command.Name);
+                await ctx.RespondAsync("raw argument string: " + ctx.RawArgumentString);
+                await ctx.RespondAsync("oof");
             }
+            else
+            {
+                
+                Leaderboard leaderboard = Program.srcClient.Leaderboards.GetLeaderboardForLevel(
+                    Program.srb2Game.ID,
+                    levelID, // GF1
+                    categoryID, // Sonic
+                    5
+                );
+                /*Level level = Program.srb2Game.Levels.ToArray()[1];
+                IEnumerable<Run> characterRuns = level.Runs.Where(run => run.Category.Name.ToLower() == character.ToLower()); //tolower is to make it case insensitive
+                Run[] verifiedRuns = characterRuns.Where(run => run.Status.Type == RunStatusType.Verified).OrderBy(run => run.Times.GameTime.Value).ToArray();
+                for (int i = 0; i < leaderboard.Records.Count(); i++)
+                {
+                    await ctx.RespondAsync($"{i + 1}. {leaderboard.Records[i].Player.Name} | {leaderboard.Records[i].Times.GameTime.Value.ToString(Program.timeFormat)} : {leaderboard.Records[i].WebLink.AbsoluteUri}");
+                }
+                */
+
+                var records = new DiscordEmbedBuilder
+                {
+                    Title = leaderboard.Level.Name + " | " + leaderboard.Category.Name,
+                    Color = DiscordColor.Green,
+                    Url = leaderboard.WebLink.AbsoluteUri
+                };
+                for (int i = 0; i < leaderboard.Records.Count(); i++)
+                {
+                    records.AddField($"{i + 1}. {leaderboard.Records[i].Player.Name} | {leaderboard.Records[i].Times.GameTime.Value.ToString(Program.timeFormat)} ", 
+                    leaderboard.Records[i].WebLink.AbsoluteUri);
+
+                   
+                }
+
+                await ctx.RespondAsync(embed: records);
+
+            }
+
         }
 
-        [Command("GF2")]
+        [Command("GFZ2")]
         public async Task Greenflower2Records(CommandContext ctx, string character)
         { 
 
--- Program.cs
+++ Program.cs
@@ -15,14 +15,77 @@ using SpeedrunComSharp;
 
 namespace RoboBot
 {
+    #region Level and Characters Enums
     public static class SRB2Enums
     {
         public static Dictionary<string, string> levelsID = new Dictionary<string, string>()
         {
-            {"GF1", "ywejpqld"},
-            {"GF2", "69zq08x9"},
-            {"GF3", "r9gj87j9"},
-            {"TH1", "o9xz0x3w"}
+            {"GFZ1", "ywejpqld"},
+            {"GFZ2", "69zq08x9"},
+            {"GFZ3", "r9gj87j9"},
+
+            {"THZ1", "o9xz0x3w"},
+            {"THZ2", "495rnmmd"},
+            {"THZ3", "rdq3qnow"},
+
+            {"DSZ1", "5d7er2qw"},
+            {"DSZ2", "kwj8rv09"},
+            {"DSZ3", "owoex3jd"},
+
+            {"CEZ1", "xd1314z9"},
+            {"CEZ2", "ewpz58y9"},
+            {"CEZ3", "y9mrz2zw"},
+
+            {"ACZ1", "5wknlqvw"},
+            {"ACZ2", "5928p479"},
+            {"ACZ3", "29vlpzqw"},
+
+            {"RVZ1", "xd4y8kqd"},
+
+            {"ERZ1", "xd02o4m9"},
+            {"ERZ2", "rw6lympw"},
+
+            {"BCZ1", "z98zl5r9"},
+            {"BCZ2", "rdnlgx5w"},
+            {"BCZ3", "ldy50kpw"},
+
+            {"FHZ", "gdr87oe9"},
+
+            {"PTZ", "nwlyezo9"},
+
+            {"FFZ", "ywejp3ld"},
+
+            {"TLZ", "69zq06x9"},
+
+            {"HHZ", "r9gj8nj9"},
+
+            {"AGZ", "o9xz0o3w"},
+
+            {"ATZ", "495rnzmd"},
+
+            {"SS1", "rdq3qoow"},
+
+            {"SS2", "5d7erxqw"},
+
+            {"SS3", "kwj8r609"},
+
+            {"SS4", "owoex5jd"},
+
+            {"SS5", "xd131yz9"},
+
+            {"SS6", "ewpz5oy9"},
+
+            {"SS7", "y9mrzgzw"},
+
+            {"BHZ", "5wknlevw"},
+
+            {"CCZ", "5928pr79"},
+
+            {"DHZ", "29vlp0qw"},
+
+            {"APZ1", "xd4y8eqd"},
+
+            {"APZ2", "xd02onm9"}
         };
         public static Dictionary<string, string> categoriesID = new Dictionary<string, string>()
         {
@@ -31,13 +94,14 @@ namespace RoboBot
             {"knuckles", "9kvoq882"},
             {"amy", "rkll95nk"},
             {"fang", "ndx9e3rd"},
-            {"metal sonic", "w20g8q5k"}
+            {"metal", "w20g8q5k"}
         };
     }
+    #endregion
     class Program
     {
         public static string timeFormat = @"mm\:ss\.ff";
-        static DiscordClient discord;
+        public static DiscordClient discord;
         static CommandsNextModule commands;
         
         public static Game srb2Game;
