b9f9c7801f06c9cf171fd3cfd9b07ddebea75873
Refrag <R3FR4GYT@gmail.com>
2021-08-08 18:09:24 +0200

    Check all the ReactionMessages for changes when the bot starts up + minor code refactoring

--- ReactionInteractions.cs
+++ ReactionInteractions.cs
@@ -6,17 +6,20 @@ using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
+using DSharpPlus.Exceptions;
 using Newtonsoft.Json;
 
 namespace RoboBot
 {
     public class ReactionInteractions
     {
-        private readonly string ReactionMessagesLocalPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "reaction-messages.json");
-        
+        private readonly string ReactionMessagesLocalPath =
+            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "reaction-messages.json");
+
         private DiscordClient _client;
 
         public List<ReactionMessage> ReactionMessages;
+
         public ReactionInteractions(DiscordClient client)
         {
             _client = client;
@@ -24,6 +27,7 @@ namespace RoboBot
             _client.MessageReactionRemoved += DiscordOnMessageReactionRemoved;
 
             LoadReactionMessages();
+            CheckAllReactionMessages().Wait();
         }
         
         private Task DiscordOnMessageReactionAdded(DiscordClient sender, MessageReactionAddEventArgs e)
@@ -37,8 +41,8 @@ namespace RoboBot
                     e.Message.RespondAsync("nothing associated, do nothing");
                     return;
                 }
-                
-                if (e.User.IsBot)
+
+                if (e.User.Equals(Program.discord.CurrentUser))
                     return;
 
                 if (!associatedMessage.Rules.TryGetValue(e.Emoji, out DiscordRole roleToGrant))
@@ -63,8 +67,8 @@ namespace RoboBot
                     e.Message.RespondAsync("nothing associated, do nothing");
                     return;
                 }
-                
-                if (e.User.IsBot)
+
+                if (e.User.Equals(Program.discord.CurrentUser))
                     return;
 
                 if (!associatedMessage.Rules.TryGetValue(e.Emoji, out DiscordRole roleToGrant))
@@ -101,11 +105,61 @@ namespace RoboBot
             }
             
             string json = File.ReadAllText(ReactionMessagesLocalPath);
-            List<SerializedReactionMessage> serializedReactionMessages = JsonConvert.DeserializeObject<List<SerializedReactionMessage>>(json);
+            List<SerializedReactionMessage> serializedReactionMessages =
+                JsonConvert.DeserializeObject<List<SerializedReactionMessage>>(json);
 
             foreach (SerializedReactionMessage serializedReactionMessage in serializedReactionMessages)
             {
-                ReactionMessages.Add(serializedReactionMessage.ToReactionMessage());
+                ReactionMessage reactionMessage = serializedReactionMessage.ToReactionMessage();
+
+                if (reactionMessage != null)
+                    ReactionMessages.Add(reactionMessage);
+            }
+        }
+
+        public async Task CheckAllReactionMessages()
+        {
+            foreach (ReactionMessage reactionMessage in ReactionMessages)
+            {
+                await CheckAllRules(reactionMessage);
+            }
+        }
+
+        public async Task CheckAllRules(ReactionMessage reactionMessage)
+        {
+            IReadOnlyCollection<DiscordMember> guildMembers = await reactionMessage.Message.Channel.Guild.GetAllMembersAsync();
+            
+            foreach (KeyValuePair<DiscordEmoji, DiscordRole> rule in reactionMessage.Rules)
+            {
+                IEnumerable<DiscordUser> reactedMembers = reactionMessage.Message.GetReactionsAsync(rule.Key, 500).Result;
+
+                foreach (DiscordMember member in guildMembers)
+                {
+                    if (member.Equals(Program.discord.CurrentUser))
+                        continue;
+                    
+                    bool reacted = false;
+                    bool hasRole = false;
+
+                    if (reactedMembers.FirstOrDefault(x => x.Equals(member)) != null)
+                        reacted = true;
+
+                    if (member.Roles.Contains(rule.Value))
+                        hasRole = true;
+                        
+                    if ((reacted && hasRole) || (!reacted && !hasRole))
+                        continue;
+
+                    if (reacted)
+                    {
+                        Console.WriteLine(member.Username + " grant " + rule.Value.Name);
+                        await member.GrantRoleAsync(rule.Value);
+                        continue;
+                    }
+
+                    Console.WriteLine(member.Username + " revoke " + rule.Value.Name);
+                    await member.RevokeRoleAsync(rule.Value);
+                }
             }
         }
     }
